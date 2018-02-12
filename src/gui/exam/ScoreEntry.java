242f28b5f8e5774950129cc7bad25e25d9d592f2
Alex Lübeck PC2 <omega.zero@outlook.com>
2018-02-12 13:49:20 +0100

    improving the format in the scoreEntry list

--- src/gui/exam/ScoreEntry.java
+++ src/gui/exam/ScoreEntry.java
@@ -3,15 +3,20 @@ package gui.exam;
 class ScoreEntry
 {
     private String team;
+
     private int score;
-    
+
+    private int toreHome, toreOpp;
+
+    private int countWin, countTie, countLoss;
+
     public ScoreEntry(String team, int goalsActive, int goalsPassive)
     {
-        if(team == null || team.length() == 0)
+        if (team == null || team.length() == 0)
         {
             throw new IllegalArgumentException("Ungültige Team-Kennung");
         }
-        if(goalsActive < 0 || goalsPassive < 0)
+        if (goalsActive < 0 || goalsPassive < 0)
         {
             throw new IllegalArgumentException("Negative Torezahl");
         }
@@ -21,32 +26,75 @@ class ScoreEntry
 
     private void update(int gA, int gP)
     {
-        if(gA > gP)
+        toreHome += gA;
+        toreOpp += gP;
+        if (gA > gP)
         {
             score += 3;
+            countWin++;
         }
-        else if(gA == gP)
+        else if (gA == gP)
         {
             score += 1;
+            countTie++;
+        }
+        else
+        {
+            countLoss++;
         }
     }
-    
+
     public void add(ScoreEntry se)
     {
-        if(!team.equals(se.team))
+        if (!team.equals(se.team))
         {
             return;
         }
         score += se.score;
+        toreHome += se.toreHome;
+        toreOpp += se.toreOpp;
+
+        if (se.toreHome > se.toreOpp)
+        {
+            score += 3;
+            countWin++;
+        }
+        else if (se.toreHome == se.toreOpp)
+        {
+            score += 1;
+            countTie++;
+        }
+        else
+        {
+            countLoss++;
+        }
+
     }
-    
+
     public void subtract(ScoreEntry se)
     {
-        if(!team.equals(se.team))
+        if (!team.equals(se.team))
         {
             return;
         }
         score -= se.score;
+        toreHome -= se.toreHome;
+        toreOpp -= se.toreOpp;
+
+        if (se.toreHome > se.toreOpp)
+        {
+            score += 3;
+            countWin++;
+        }
+        else if (se.toreHome == se.toreOpp)
+        {
+            score += 1;
+            countTie++;
+        }
+        else
+        {
+            countLoss++;
+        }
     }
 
     public String getTeam()
@@ -58,19 +106,19 @@ class ScoreEntry
     {
         return score;
     }
-    
+
     @Override
     public boolean equals(Object o)
     {
-        if(!(o instanceof ScoreEntry))
+        if (!(o instanceof ScoreEntry))
         {
             return false;
         }
-        return team.equals(((ScoreEntry)o).team);
+        return team.equals(((ScoreEntry) o).team);
     }
-    
+
     public String toString()
     {
-        return team + ": " + score;
+        return team + ": " + score + " (" + toreHome + ":" + toreOpp + " " + countWin + "/" + countTie + "/" + countLoss + ")";
     }
 }
